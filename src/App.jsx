440a5b0ec073d996b8a8920737b3aa11b719a3a1
RubyGorgym <rubigorgy@gmail.com>
2022-10-25 13:58:44 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div className="App">
+      <button onClick={() => setCounter(counter + 1)}>+</button>
+      <span>{counter}</span>
+      <button onClick={() => setCounter(counter - 1)}>-</button>
+    </div>
+  );
+}
+
+export default App;
